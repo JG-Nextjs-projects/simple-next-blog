cf6891cb0ef7f1021584a4682f50bd41e584af6b
jsguerra <jose@gcostudios.com>
2022-03-21 17:25:24 +0000

    updated logic

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,7 +1,64 @@
-import React from 'react';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import Head from 'next/head';
+import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote';
+import { serialize } from 'next-mdx-remote/serialize';
 
-const Post = () => {
-  return <div>The Post</div>;
+import Layout from '../../components/layout/Layout';
+import { IPost } from '../../types/post';
+import { getAllPosts, getPost } from '../../utils/mdxUtils';
+
+type Props = {
+  source: MDXRemoteSerializeResult;
+  frontMatter: Omit<IPost, 'slug'>;
 };
 
-export default Post;
+const PostPage: React.FC<Props> = ({ source, frontMatter }: Props) => {
+  return (
+    <Layout>
+      <Head>
+        <meta name="description" content={frontMatter.description} key="description" />
+        <meta property="og:description" content={frontMatter.description} key="ogDescription" />
+        <meta property="og:image" content="" key="ogImage" />
+      </Head>
+
+      <article className="prose prose-green">
+        <header>
+          <h1>{frontMatter.title}</h1>
+          <p>{frontMatter.description}</p>
+        </header>
+
+        <MDXRemote {...source} />
+      </article>
+    </Layout>
+  );
+};
+
+export default PostPage;
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { content, data } = getPost(params?.slug as string);
+
+  const mdxSource = await serialize(content, { scope: data });
+
+  return {
+    props: {
+      source: mdxSource,
+      frontMatter: data,
+    },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const posts = getAllPosts(['slug']);
+
+  const paths = posts.map((post) => ({
+    params: {
+      slug: post.slug,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};

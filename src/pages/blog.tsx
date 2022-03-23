dce0778e8f257686abc8ea6806832c3b07254545
jsguerra <jose@gcostudios.com>
2022-03-23 18:28:08 +0000

    added post map logic

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,14 +1,39 @@
+import { GetStaticProps } from 'next';
+import Link from 'next/link';
 import type { ReactElement } from 'react';
 import React from 'react';
 
 import Layout from '../components/layout/Layout';
 import Styles from '../styles/Blog.module.css';
+import { IPost } from '../types/post';
+import { getAllPosts } from '../utils/mdxUtils';
 
-const Blog = () => {
+type Props = {
+  posts: IPost[];
+};
+
+const Blog = ({ posts }: Props) => {
   return (
-    <div className={Styles.container}>
-      <h1>Blog</h1>
-    </div>
+    <>
+      <div className={Styles.container}>
+        <h1 style={{ marginBottom: '3rem' }}>Blog</h1>
+
+        {posts.map((post) => (
+          <div key={post.slug}>
+            <h2>
+              <Link href={`post/${post.slug}`}>
+                <a>{post.title}</a>
+              </Link>
+            </h2>
+            <time>{post.date}</time>
+            <p>{post.description}</p>
+            <Link href={`post/${post.slug}`}>
+              <a>Read more</a>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
@@ -16,4 +41,14 @@ export default Blog;
 
 Blog.getLayout = function getLayout(page: ReactElement) {
   return <Layout>{page}</Layout>;
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const posts = getAllPosts(['slug', 'date', 'title', 'description']);
+
+  return {
+    props: {
+      posts,
+    },
+  };
 };
